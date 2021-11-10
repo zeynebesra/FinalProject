63db542dee5a918f81595ba6adab7b2e5656fba5
Zeynebesra <zeynep.ozturk@mobilion.com.tr>
2021-11-10 13:58:20 +0300

    Add project files.

---
+++ DataAccess/Abstract/IProductDal.cs
@@ -0,0 +1,24 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    //Productla ilgili veritabanında yapılacak operasyonları içeren interface
+    //
+
+    public interface IProductDal
+    {
+        List<Product> GetAll();
+        void Add(Product product);
+        void Update(Product product);
+        void Delete(Product product);
+
+        List<Product> GetAllByCategory(int categoryId);
+
+
+    }
+}
